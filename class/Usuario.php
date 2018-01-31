3bdd74352ab1bc8e3a7302f67ec86fbea4cb7e21
Pedro Zanon <phzanon>
2018-01-30 23:16:01 -0200

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -49,7 +49,7 @@
 
 		public function loadById($id)
 		{
-			echo "<br>".$id;
+			//echo "<br>".$id;
 			$sql = new Sql();
 			$res = $sql->select("select * from tb_usuarios where idusuario = :ID", array(":ID"=>$id));
 
@@ -61,6 +61,40 @@
 				$this->setDeslogin($row['deslogin']);
 				$this->setDessenha($row['dessenha']);
 				$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			}
+		}
+
+		public static function getList()
+		{
+			$sql = new Sql();
+			return $sql->select("select * from tb_usuarios order by deslogin");
+		}
+
+		public static function search($login)
+		{
+			$sql = new Sql();
+			return $sql->select("select * from tb_usuarios where deslogin like :SEARCH order by deslogin",array(':SEARCH'=>"%".$login."%"));
+		}
+
+
+		public function login($login, $password)
+		{
+			$sql = new Sql();
+			$res = $sql->select("select * from tb_usuarios where deslogin = :LOGIN and dessenha = :PASSWORD", array(":LOGIN"=>$login,
+				":PASSWORD"=>$password));
+
+			//if(isset($res[0]))
+			if(count($res) > 0)
+			{
+				$row = $res[0];
+				$this->setIdUsuario($row['idusuario']);
+				$this->setDeslogin($row['deslogin']);
+				$this->setDessenha($row['dessenha']);
+				$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			}
+			else
+			{
+				throw new Exception("Login ou senha invalidos");
 			}
 		}
 
--- index.php
+++ index.php
@@ -2,10 +2,25 @@
 
 	require_once("config.php");
 
-	echo"oi";
-	$root = new Usuario();
-	echo"oi2";
-	$root->loadById(3);
-	echo $root;
+	//echo"oi";
+	//carrega um usuario
+	//$root = new Usuario();
+	//echo"oi2";
+	//$root->loadById(3);
+	//echo $root;
 
+
+	//carrega uma lista de usuarios
+	//$lista = Usuario::getList();
+	//echo json_encode($lista);
+
+	//carrega uma lista de usuarios buscado pelo login
+	//$search = Usuario::search("ot");
+	//echo json_encode($search);
+
+
+	//carregando um usuario usando o login e senha
+	$usuario = new Usuario();
+	$usuario->login("root","123iug");
+	echo $usuario;
 ?>
